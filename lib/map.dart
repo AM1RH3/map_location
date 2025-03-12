bd4f55dd14c3f865441d4f2a887093357ce3d0e6
AM1RH3 <amirhossein.rasooli@outlook.com>
2025-03-12 17:17:13 +0330

    add mappppp

--- lib/map.dart
+++ lib/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class MyMapView extends StatefulWidget {
   const MyMapView({super.key});
@@ -8,8 +9,31 @@ class MyMapView extends StatefulWidget {
 }
 
 class _MyMapViewState extends State<MyMapView> {
+  final location = Location();
+  LocationData? myLocationData;
+  @override
+  void initState() {
+    super.initState();
+    initLocation();
+  }
+
+  void initLocation() async {
+    final permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.granted) {
+      myLocationData = await location.getLocation();
+    } else {
+      await location.requestPermission();
+      myLocationData = await location.getLocation();
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text('${myLocationData?.latitude},${myLocationData?.longitude}'),
+      ),
+    );
   }
 }
