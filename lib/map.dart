9e2f86c8c703e1da045dfb4a47db436753c8816a
AM1RH3 <amirhossein.rasooli@outlook.com>
2025-03-12 18:16:28 +0330

    final

--- lib/map.dart
+++ lib/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:location/location.dart';
 
 class MyMapView extends StatefulWidget {
@@ -9,12 +10,24 @@ class MyMapView extends StatefulWidget {
 }
 
 class _MyMapViewState extends State<MyMapView> {
+  late MapController mapController;
+
   final location = Location();
   LocationData? myLocationData;
   @override
   void initState() {
     super.initState();
+    initOSM();
     initLocation();
+  }
+
+  void initOSM() async {
+    mapController = MapController(
+        // initMapWithUserPosition: UserTrackingOption(enableTracking: true),
+        initPosition: GeoPoint(
+      latitude: myLocationData?.latitude ?? 0.0,
+      longitude: myLocationData?.longitude ?? 0.0,
+    ));
   }
 
   void initLocation() async {
@@ -26,13 +39,29 @@ class _MyMapViewState extends State<MyMapView> {
       myLocationData = await location.getLocation();
     }
     setState(() {});
+    final myGeoPoint = GeoPoint(
+        latitude: myLocationData!.latitude!,
+        longitude: myLocationData!.longitude!);
+    // ignore: deprecated_member_use
+    await mapController.goToLocation(myGeoPoint);
+    await mapController.addMarker(myGeoPoint,
+        markerIcon: const MarkerIcon(
+          icon: Icon(
+            Icons.location_on_outlined,
+            size: 40,
+            color: Colors.red,
+          ),
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('${myLocationData?.latitude},${myLocationData?.longitude}'),
+      body: OSMFlutter(
+        controller: mapController,
+        osmOption: OSMOption(
+          zoomOption: ZoomOption(initZoom: 15),
+        ),
       ),
     );
   }
