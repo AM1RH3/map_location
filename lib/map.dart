c907810a1f4acd664834fc4eeb3f5e49a30270bd
AM1RH3 <amirhossein.rasooli@outlook.com>
2025-03-12 15:19:50 +0330

    first commit

---
+++ lib/map.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyMapView extends StatefulWidget {
+  const MyMapView({super.key});
+
+  @override
+  State<MyMapView> createState() => _MyMapViewState();
+}
+
+class _MyMapViewState extends State<MyMapView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
